d2d3c779aea315396bb20d77dfa3985904aac151
Dmitriy Gogolev <gogolevdms@gmail.com>
2022-01-17 00:19:16 +0300

    fix config

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,9 +20,25 @@ module.exports = {
       url: process.env.ETHERIUM_MAINNET_URL,
       accounts: [process.env.PRIVATE_KEY]
     },
-    bsctestnet: {
-      url: process.env.ETHERIUM_BSCTESTNET_URL,
+    rinkeby: {
+      url: process.env.ETHERIUM_RINKEBY_URL,
       accounts: [process.env.PRIVATE_KEY]
+    },
+    bsc: {
+      url: process.env.BSC_MAINNET_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      gasLimit: 8000000,
+      network_id: "56",
+      confirmations: 7,
+      skipDryRun: true
+    },
+    bsctestnet: {
+      url: process.env.BSC_TESTNET_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      gasLimit: 8000000,
+      network_id: "97",
+      confirmations: 7,
+      skipDryRun: true
     },
   },
   etherscan: {
